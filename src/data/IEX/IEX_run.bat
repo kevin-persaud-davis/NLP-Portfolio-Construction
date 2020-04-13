e6db951909d645d1dbe2fe03f1193ac811c0c767
Kevin <kevinjpn@gmail.com>
2020-04-13 11:58:04 -0400

    update bat file

--- src/data/IEX/IEX_run.bat
+++ src/data/IEX/IEX_run.bat
@@ -1,3 +1,3 @@
 @echo off
 
-python c:\Users\kpdav\.Github_projects\portfolio-construction\NLP-Portfolio-Construction\src\data\IEX\IEX_price.py %*
+python c:\Users\kpdav\Github_projects\portfolio-construction\NLP-Portfolio-Construction\src\data\IEX\IEX_price.py %*
